35359e184d7f06bdfc9605514221edadf2462892
Squizyton <fallendemongod@gmail.com>
2020-03-05 12:25:11 -0500

    BaseProject

---
+++ Game/Assets/UIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+
+
+
+    // Start is called before the first frame update
+
+    public GameObject LoginButton, CreateAccountButton, LoginFields, CreateAccountFields;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    public void Login()
+    {
+        LoginButton.SetActive(false);
+        CreateAccountButton.SetActive(false);
+        LoginFields.SetActive(true);
+    }
+
+
+    public void CreateAccount()
+    {
+        LoginButton.SetActive(false);
+        CreateAccountButton.SetActive(false);
+        CreateAccountFields.SetActive(true);
+    }
+
+}
